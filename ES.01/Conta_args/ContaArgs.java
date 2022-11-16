37481ff77ebd495a960dbd639b52ed142c58db9c
samuelelc <98475453+samuelelc@users.noreply.github.com>
2022-11-16 09:22:30 +0100

    Delete ES.01 directory

--- ES.01/Conta_args/ContaArgs.java
+++
@@ -1,6 +0,0 @@
-public class ContaArgs{
-	public static void main(String args[]){
-		System.out.print("Numero argomenti inseriti: ");
-		System.out.println(args.length);
-	}
-}
--- ES.01/ReversePrintArgs/ReversePrintArgs.java
+++
@@ -1,7 +0,0 @@
-public class ReversePrintArgs{
-	public static void main(String args[]){
-		for(int i = args.length - 1; i >= 0 ; i--){
-			System.out.println(args[i]);
-		}
-	}
-}
