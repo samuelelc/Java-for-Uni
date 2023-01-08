558c07e63854243a22a43a7509705e6cd06b4653
samuelelc <98475453+samuelelc@users.noreply.github.com>
2023-01-08 19:17:16 +0100

    Delete ES.09/es09src directory

--- ES.09/es09src/Aritmetica.java
+++
@@ -1,6 +0,0 @@
-package prg.es09;
-
-public interface Aritmetica{
-	public Numero somma(Numero n);
-	public Numero sottrai(Numero n);
-}
--- ES.09/es09src/Numero.java
+++
@@ -1,5 +0,0 @@
-package prg.es09;
-
-public abstract class Numero implements Aritmetica{
-	//eredita i metodi astratti di Aritmetica
-}
